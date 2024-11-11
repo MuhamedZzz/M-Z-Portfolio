2b04e5dc30e434b2d3e72254133504bc088edeba
MuhamedZzz <muhamedzimeri5@gmail.com>
2024-11-11 01:21:31 +0100

    initialize

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const goalsModals = document.querySelectorAll(".goals-modal, .projects-modal");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+  goalsModals[modalClick].classList.add("active");
+};
+
+learnMoreBtns.forEach((learnMoreBtns, i) => {
+  learnMoreBtns.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+  modalCloseBtns.addEventListener("click", () => {
+    goalsModals.forEach((modalView) => {
+      modalView.classList.remove("active");
+    });
+  });
+});
